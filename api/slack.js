8d0280ee02169543b546019adafb0cdd70ef9801
chiharu <chiharu@chiharunoMacBook-Pro.local>
2026-05-18 17:32:09 +0900

    fix event check

--- api/slack.js
+++ api/slack.js
@@ -15,42 +15,30 @@ module.exports = async function handler(req, res) {
   try { body = JSON.parse(rawBody); }
   catch { return res.status(400).send('Bad Request'); }
 
-  console.log('BODY TYPE:', body && body.type);
-  console.log('EVENT:', JSON.stringify(body.event));
-
   if (body.type === 'url_verification') {
     return res.status(200).json({ challenge: body.challenge });
   }
 
-  const timestamp = req.headers['x-slack-request-timestamp'];
-  const signature = req.headers['x-slack-signature'];
-  if (timestamp && signature) {
-    const sigBase = 'v0:' + timestamp + ':' + rawBody;
-    const mySig = 'v0=' + crypto
-      .createHmac('sha256', process.env.SLACK_SIGNING_SECRET)
-      .update(sigBase, 'utf8')
-      .digest('hex');
-    try {
-      if (!crypto.timingSafeEqual(Buffer.from(mySig), Buffer.from(signature))) {
-        return res.status(401).send('Unauthorized');
-      }
-    } catch(e) { return res.status(401).send('Unauthorized'); }
-  }
-
   const event = body.event;
-  if (!event || event.type !== 'message' || event.bot_id || !event.files) {
+  console.log('EVENT TYPE:', event && event.type);
+  console.log('SUBTYPE:', event && event.subtype);
+  console.log('HAS FILES:', event && event.files && event.files.length);
+  console.log('BOT ID:', event && event.bot_id);
+
+  if (!event || !event.files) {
     return res.status(200).send('OK');
   }
 
   const audioFile = event.files.find(function(f) {
     return ['mp3', 'm4a', 'mp4', 'wav', 'webm'].includes(f.filetype);
   });
+
+  console.log('AUDIO FILE:', audioFile && audioFile.name);
+
   if (!audioFile) return res.status(200).send('OK');
 
   const channel = event.channel;
   const token = process.env.SLACK_BOT_TOKEN;
-
-  res.status(200).send('OK');
 
   const toolUrl = 'https://chiharuf333.github.io/counseling-tool/?audio='
     + encodeURIComponent(audioFile.url_private)
@@ -79,4 +67,6 @@ module.exports = async function handler(req, res) {
       }
     ]
   }, { headers: { Authorization: 'Bearer ' + token } });
+
+  return res.status(200).send('OK');
 };
